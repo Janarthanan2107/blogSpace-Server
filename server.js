083dd02d70e18801778d913d1db81da025cd7e8f
Janarthanan2107 <131163725+Janarthanan2107@users.noreply.github.com>
2024-07-31 16:58:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import admin from "firebase-admin";
-import serviceAccountKey from "./blog-space-user-firebase-adminsdk-nx8my-487b993063.json" assert { type: "json" };
+import fs from 'fs';
+import path from 'path';
+
+const serviceAccountPath = path.resolve('./blog-space-user-firebase-adminsdk-nx8my-487b993063.json');
+const serviceAccountKey = JSON.parse(fs.readFileSync(serviceAccountPath, 'utf8'));
 
 // service
 import connectDB from "./config/db.js";
