c0e926d2216595263c930cb0a002f64a758fce87
Janarthanan2107 <131163725+Janarthanan2107@users.noreply.github.com>
2024-08-14 08:02:25 +0530

    Update notificationRoute.js

--- routes/notificationRoute.js
+++ routes/notificationRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { likeBlog, isLikedByUser } from "../controllers/notifyController.js";
+import { likeBlog, isLikedByUser, getNewNotification } from "../controllers/notifyController.js";
 
 // middleware
 import { verifyJWT } from "../middleware/verifyJWT.js"
@@ -8,6 +8,7 @@ const router = express.Router();
 
 router.post("/like", verifyJWT, likeBlog)
 router.post("/isLikedByUser", verifyJWT, isLikedByUser)
+router.get("/new-notification", verifyJWT, getNewNotification)
 
 
 export default router;
